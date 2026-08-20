66203a63113faf9eb882e3651ef20ec97e5d438e
Tomoaki Hirabayashi <80768507+avaice@users.noreply.github.com>
2026-08-20 22:22:37 +0900

    add linked list answer stub

---
+++ questions/007_linked_list_lru_cache/ans.ts
@@ -0,0 +1,16 @@
+export class LRUCache {
+  constructor(capacity: number) {
+    void capacity;
+  }
+
+  get(key: number): number | null {
+    void key;
+    throw new Error("TODO: implement LRUCache.get");
+  }
+
+  put(key: number, value: number): void {
+    void key;
+    void value;
+    throw new Error("TODO: implement LRUCache.put");
+  }
+}
