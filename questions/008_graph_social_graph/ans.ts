8d2dfd1dd3ea8ab3ed0dcb23c8c33c35859bef82
Tomoaki Hirabayashi <80768507+avaice@users.noreply.github.com>
2026-08-20 22:23:04 +0900

    add graph answer stub

---
+++ questions/008_graph_social_graph/ans.ts
@@ -0,0 +1,19 @@
+export class SocialGraph {
+  addFriend(userA: string, userB: string): void {
+    void userA;
+    void userB;
+    throw new Error("TODO: implement SocialGraph.addFriend");
+  }
+
+  canReach(fromUser: string, toUser: string): boolean {
+    void fromUser;
+    void toUser;
+    throw new Error("TODO: implement SocialGraph.canReach");
+  }
+
+  getDistance(fromUser: string, toUser: string): number | null {
+    void fromUser;
+    void toUser;
+    throw new Error("TODO: implement SocialGraph.getDistance");
+  }
+}
